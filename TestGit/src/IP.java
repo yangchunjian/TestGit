e7d896fd3de7748fedbcc184226b4ecf67f30af6
yangchunwei <1091938307@qq.com>
2015-04-15 17:12:04 +0800

    first test git

---
+++ TestGit/src/IP.java
@@ -0,0 +1,33 @@
+
+public class IP {
+
+	private String ip1;
+	private String ip2;
+	private String ip3;
+	private String ip4;
+	public String getIp1() {
+		return ip1;
+	}
+	public void setIp1(String ip1) {
+		this.ip1 = ip1;
+	}
+	public String getIp2() {
+		return ip2;
+	}
+	public void setIp2(String ip2) {
+		this.ip2 = ip2;
+	}
+	public String getIp3() {
+		return ip3;
+	}
+	public void setIp3(String ip3) {
+		this.ip3 = ip3;
+	}
+	public String getIp4() {
+		return ip4;
+	}
+	public void setIp4(String ip4) {
+		this.ip4 = ip4;
+	}
+	
+}
