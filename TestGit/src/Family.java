9b9769568845264dd7c8c732d02b9e7349ddc37f
yangchunwei <1091938307@qq.com>
2015-04-16 11:15:46 +0800

    Ad Family

---
+++ TestGit/src/Family.java
@@ -0,0 +1,19 @@
+
+public class Family {
+
+	private int sum;
+	private String name;
+	public int getSum() {
+		return sum;
+	}
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
